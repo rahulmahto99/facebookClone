2865de0afc433a1b6d03b2321c14baeb12ddcdd4
Rahul Mahto <118905813+rahulmahto99@users.noreply.github.com>
2023-08-18 10:08:36 +0530

    Hii

---
+++ Script.js
@@ -0,0 +1,12 @@
+// Nav profile menu Toggle start here 
+
+
+let settingMenu= document.querySelector(".setting-menu");
+
+function settingMenuTogglebtn (){
+    settingMenu.classList.toggle("setting-menu-height");
+}
+
+// Nav profile menu Toggle emd here 
+
+
